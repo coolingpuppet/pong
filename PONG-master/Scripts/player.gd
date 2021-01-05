ca657ea319ba3aa628faeeac4b1b7a95511f9864
Rakib Hasan <hrakib182@gmail.com>
2021-01-06 02:13:59 +0600

    5/01/2021
    #Project Setup
    #Player Created

---
+++ PONG-master/Scripts/player.gd
@@ -0,0 +1,15 @@
+extends KinematicBody2D
+
+var SPEED = 400
+var Velocity = Vector2.ZERO
+
+
+func _physics_process(delta):
+	if Input.is_action_pressed("player_up"):
+		Velocity.y = -SPEED
+	elif Input.is_action_pressed("player_down"):
+		Velocity.y = SPEED
+	else:
+		Velocity.y = 0
+		
+	Velocity = move_and_slide(Velocity)
