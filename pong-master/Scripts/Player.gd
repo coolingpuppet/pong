0b5374ed551a00d2f80e9ef475a818f4e974b116
Rakib Hasan <hrakib182@gmail.com>
2021-01-11 23:33:18 +0600

    update(11/01/2021)

---
+++ pong-master/Scripts/Player.gd
@@ -0,0 +1,18 @@
+extends KinematicBody2D
+
+signal update
+
+export var move_speed = 400
+
+var direction = Vector2()
+
+func _ready():
+	pass
+	
+func _process(delta):
+	emit_signal("update")
+
+func _physics_process(delta):
+	if direction.length() > 0:
+		direction = direction.normalized() * move_speed
+		move_and_collide(direction*delta)
